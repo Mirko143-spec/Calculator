0df0647e0cd8c6b5fe601d9909d7355ae9b120cd
miran <miran123125@gmail.com>
2025-03-02 14:56:30 +0100

    fixed the visual aspect and added s bit of js code

--- js.js
+++ js.js
@@ -1,9 +1,62 @@
-let num = 0;
+const calculator = document.querySelector('.table')
+const keys = calculator.querySelector('.buttons')
+const display = document.querySelector('.display')
 
-function addition() {
-    document.getElementById("number").value = ++num;
-}
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target;
+        const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+        const previousKeyType = calculator.dataset.previousKeyType;
 
-function reset() {
-    document.getElementById("number").value = 0;
-}
+        if (!action) {
+            if (displayedNum === '0' || previousKeyType === 'operator') {
+                display.textContent = keyContent;
+            } else {
+                display.textContent = displayedNum + keyContent;
+            }
+        }
+
+        if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
+
+            key.classList.add('is-depressed')
+
+            calculator.dataset.previousKeyType = 'operator';
+
+            calculator.dataset.firstValue = displayedNum;
+            calculator.dataset.operator = action;
+        }
+
+        const calculate = (n1, operator, n2) => {
+            let result = ''
+
+            if (operator === 'add') {
+                result = parseInt(n1) + parseInt(n2);
+            } else if (operator === 'subtract') {
+                result = parseInt(n1) - parseInt(n2);
+            } else if (operator === 'multiply') {
+                result = parseInt(n1) * parseInt(n2);
+            } else if (operator === 'divide') {
+                result = parseInt(n1) / parseInt(n2);
+            }
+                
+            return result;
+            
+        }
+
+        Array.from(key.parentNode.children)
+            .forEach(k => k.classList.remove('is-depressed'))
+
+        if (action === 'equal') {
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayedNum;
+
+            display.textContent = calculate(firstValue, operator, secondValue);
+        }
+
+        if (action === 'clear') {
+        }
+    }
+})
