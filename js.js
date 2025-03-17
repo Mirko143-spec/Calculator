37eacdee8b3a2dbf49ec9d29784f43e852ea81f8
miran <miran123125@gmail.com>
2025-03-17 21:22:01 +0100

    Made calculator more complex

--- js.js
+++ js.js
@@ -8,10 +8,31 @@ function clearDisplay() {
     display.value = "";
 }
 
+const transform = (value, operator) => value.toLowerCase().split(operator).map(x => parseInt(x))
+
+function stripLast(value) {
+    let last = value[value.length - 1]  
+    if (["+", "-", "x", "/"].includes(last.toLowerCase())) {
+        return value.slice(0, value.length - 1)
+    }
+    return value
+}
+
 function calculate() {
-    try {
-        display.value = eval(display.value)
-    } catch (error) {
-        display.value = "Error"
+    let value = stripLast(display.value)
+    if (value.includes("+")) {
+        display.value = transform(value, "+")
+            .reduce((accumulator, currentvalue) => accumulator + currentvalue, 0)
+    } else if (value.includes("-")) {
+        display.value = transform(value, "-")
+            .reduce((accumulator, currentvalue) => accumulator - currentvalue, 0)
+    } else if (value.toLowerCase().includes("x")) {
+        let si = transform(value, "x")
+        let initial = si[0]
+            display.value = si.slice(1).reduce((accumulator, currentvalue) => accumulator * currentvalue, initial)
+    } else {
+        let ji = transform(value, "/")
+        let initial = ji[0];
+            display.value = ji.slice(1).reduce((accumulator, currentvalue) => accumulator / currentvalue, initial)
     }
 }
